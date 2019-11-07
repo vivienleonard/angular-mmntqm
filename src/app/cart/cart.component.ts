c2d0bc7aab1791b4b7873ff79614938246e1b5ec
vivienleonard <vivien.leonard@gmail.com>
2019-11-07 13:54:09 +0100

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -25,4 +25,11 @@ export class CartComponent implements OnInit {
 
   }
 
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
 }
